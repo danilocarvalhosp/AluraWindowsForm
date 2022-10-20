b438b54ae977bfe42455819da4c20ae79e616240
Danilo Carvalho <danilocarvalhosp@gmail.com>
2022-10-20 18:58:37 -0300

    Verificando se o texto está vazio

--- CursoWindowsForm/CursoWindowsForm/frm_ValidaCPF2.cs
+++ CursoWindowsForm/CursoWindowsForm/frm_ValidaCPF2.cs
@@ -24,17 +24,28 @@ namespace CursoWindowsForm
 
         private void btn_valida_Click(object sender, EventArgs e)
         {
-            if (MessageBox.Show("Você deseja realmente validar o CPF?", "Mensagem de Validação", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            string vConteudo = msk_CPF.Text;
+            vConteudo = vConteudo.Replace(".", "").Replace("-", "");
+            vConteudo = vConteudo.Trim();
+
+            if (vConteudo == "")
             {
-                bool validaCPF = false;
-                validaCPF = Cls_Uteis.Valida(msk_CPF.Text);
-                if (validaCPF)
+                MessageBox.Show("Você deve digitar um CPF", "Mensagem de Validação", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (MessageBox.Show("Você deseja realmente validar o CPF?", "Mensagem de Validação", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
                 {
-                    MessageBox.Show("CPF VÁLIDO", "Mensagem de Validação", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-                else
-                {
-                    MessageBox.Show("CPF INVÁLIDO", "Mensagem de Validação", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    bool validaCPF = false;
+                    validaCPF = Cls_Uteis.Valida(msk_CPF.Text);
+                    if (validaCPF)
+                    {
+                        MessageBox.Show("CPF VÁLIDO", "Mensagem de Validação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("CPF INVÁLIDO", "Mensagem de Validação", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
                 }
             }
         }
