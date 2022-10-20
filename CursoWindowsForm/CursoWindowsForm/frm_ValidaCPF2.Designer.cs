f916d4e51383bff20a197e1296ad84f3f6e7b34e
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-10-20 16:56:08 -0300

    Duplicando um formulário

---
+++ CursoWindowsForm/CursoWindowsForm/frm_ValidaCPF2.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace CursoWindowsForm
+{
+    partial class frm_ValidaCPF2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.msk_CPF = new System.Windows.Forms.MaskedTextBox();
+            this.lbl_resultado = new System.Windows.Forms.Label();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.btn_valida = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // msk_CPF
+            // 
+            this.msk_CPF.Location = new System.Drawing.Point(12, 45);
+            this.msk_CPF.Mask = "000,000,000-00";
+            this.msk_CPF.Name = "msk_CPF";
+            this.msk_CPF.Size = new System.Drawing.Size(218, 23);
+            this.msk_CPF.TabIndex = 0;
+            // 
+            // lbl_resultado
+            // 
+            this.lbl_resultado.AutoSize = true;
+            this.lbl_resultado.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_resultado.Location = new System.Drawing.Point(12, 90);
+            this.lbl_resultado.Name = "lbl_resultado";
+            this.lbl_resultado.Size = new System.Drawing.Size(0, 22);
+            this.lbl_resultado.TabIndex = 1;
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Location = new System.Drawing.Point(245, 42);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(111, 23);
+            this.btn_reset.TabIndex = 2;
+            this.btn_reset.Text = "Limpa";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            // 
+            // btn_valida
+            // 
+            this.btn_valida.Location = new System.Drawing.Point(245, 71);
+            this.btn_valida.Name = "btn_valida";
+            this.btn_valida.Size = new System.Drawing.Size(111, 23);
+            this.btn_valida.TabIndex = 3;
+            this.btn_valida.Text = "Valida";
+            this.btn_valida.UseVisualStyleBackColor = true;
+            // 
+            // frm_ValidaCPF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 154);
+            this.Controls.Add(this.btn_valida);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.lbl_resultado);
+            this.Controls.Add(this.msk_CPF);
+            this.Name = "frm_ValidaCPF2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Validação CPF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaskedTextBox msk_CPF;
+        private Label lbl_resultado;
+        private Button btn_reset;
+        private Button btn_valida;
+    }
+}
