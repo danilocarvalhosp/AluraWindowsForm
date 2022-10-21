06c58ba3f44e26386e707bb6523b2901ad77751d
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-10-21 12:47:13 -0300

    Formulários MDI

---
+++ CursoWindowsForm/CursoWindowsForm/frm_Principal_Menu_MDI.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForm
+{
+    public partial class frm_Principal_Menu_MDI : Form
+    {
+        public frm_Principal_Menu_MDI()
+        {
+            InitializeComponent();
+        }
+
+        private void demonstraçãoKeyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_DemonstracaoKey f = new();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void helloWorldToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_HelloWorld f = new();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void máscaraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_Mascara f = new();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void validaCPFToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_ValidaCPF f = new();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void validaCPF2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_ValidaCPF2 f = new();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void validaSenhaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_ValidaSenha f = new();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
