e379958d887b21603d51b4900c140d344a733569
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-10-27 15:08:09 -0300

    Cursor

---
+++ CursoWindowsForm/CursoWindowsForm/frm_MouseCursor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForm
+{
+    public partial class frm_MouseCursor : Form
+    {
+        public frm_MouseCursor()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+
+            for (int i = 0; i <= 10; i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            this.Cursor = Cursors.Default;
+        }
+    }
+}
