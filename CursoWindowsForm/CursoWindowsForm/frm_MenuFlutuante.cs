231f0ef88702e2c1d12fc0e44c39e38eb0505eac
Danilo Carvalho <danilocarvalhosp@gmail.com>
2022-10-27 18:55:51 -0300

    Adicionando imagens

--- CursoWindowsForm/CursoWindowsForm/frm_MenuFlutuante.cs
+++ CursoWindowsForm/CursoWindowsForm/frm_MenuFlutuante.cs
@@ -19,25 +19,22 @@ namespace CursoWindowsForm
 
         private void frm_MenuFlutuante_MouseDown(object sender, MouseEventArgs e)
         {
-            //string str1 = e.Button.ToString();
-            //if (str1 == "Right")
-            //{
-            //    MessageBox.Show(str1);
-            //}
-
             if (e.Button == System.Windows.Forms.MouseButtons.Right)
             {
                 var ContextMenu = new ContextMenuStrip();
 
-                ContextMenu.Items.Add(DesenhaItemMenu("Item do menu 1"));
-                ContextMenu.Items.Add(DesenhaItemMenu("Item do menu 2"));
+                ContextMenu.Items.Add(DesenhaItemMenu("Item do menu 1", "key"));
+                ContextMenu.Items.Add(DesenhaItemMenu("Item do menu 2", "Frm_ValidaSenha"));
                 ContextMenu.Show(this, new Point(e.X, e.Y));
             }
 
-            ToolStripMenuItem DesenhaItemMenu(string text)
+            ToolStripMenuItem DesenhaItemMenu(string text, string nomeImagem)
             {
                 var vToolTip = new ToolStripMenuItem();
                 vToolTip.Text = text;
+
+                Image? MyImage = (Image?) Properties.Resources.ResourceManager.GetObject(nomeImagem);
+                vToolTip.Image = MyImage;
                 return vToolTip;
             }
         }
