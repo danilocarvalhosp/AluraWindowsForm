42c3ab6576fed92273ee033feb7af9c5baaf5dbe
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-10-27 15:01:04 -0300

    Capturando propriedades do mouse

---
+++ CursoWindowsForm/CursoWindowsForm/frm_MouseCaptura.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForm
+{
+    public partial class frm_MouseCaptura : Form
+    {
+        public frm_MouseCaptura()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            string str1 = e.Button.ToString();
+            MessageBox.Show("Foi pressionada o botão: " + str1);
+        }
+    }
+}
