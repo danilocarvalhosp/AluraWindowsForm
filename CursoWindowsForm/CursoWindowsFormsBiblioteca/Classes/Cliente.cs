f93f23f4f7332df26b0f6dfeced1b617e441f976
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-06-01 12:56:09 -0300

    Método de validação

--- CursoWindowsForm/CursoWindowsFormsBiblioteca/Classes/Cliente.cs
+++ CursoWindowsForm/CursoWindowsFormsBiblioteca/Classes/Cliente.cs
@@ -27,6 +27,23 @@ namespace CursoWindowsFormsBiblioteca.Classes
             public string? Telefone { get; set; }
             public string? Profissao { get; set; }
             public double RendaFamiliar { get; set; }
+
+            public void ValidaClasse()
+            {
+                ValidationContext context = new ValidationContext(this, serviceProvider: null, items: null);
+                List<ValidationResult> results = new List<ValidationResult>();
+                bool isValid = Validator.TryValidateObject(this, context, results, true);
+
+                if (isValid == false)
+                {
+                    StringBuilder sbrErrors = new StringBuilder();
+                    foreach (var validationResult in results)
+                    {
+                        sbrErrors.AppendLine(validationResult.ErrorMessage);
+                    }
+                    throw new ValidationException(sbrErrors.ToString());
+                }
+            }
         }
 
         public class List
