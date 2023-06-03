160ae34cb988ab02280d31bd67b4e8e5fd52ec2c
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-06-03 11:59:42 -0300

    Criando a classe Fichário

---
+++ CursoWindowsForm/CursoWindowsFormsBiblioteca/Classes/Databases/Fichario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CursoWindowsFormsBiblioteca.Classes.Databases
+{
+    public class Fichario
+    {
+        public string? diretorio;
+        public string mensagem;
+        public bool status;
+
+        public Fichario(string Diretorio)
+        {
+            status = true;
+            try
+            {
+                if (!Directory.Exists(Diretorio))
+                {
+                    Directory.CreateDirectory(Diretorio);
+                }
+
+                diretorio = Diretorio;
+                mensagem = "Conexão com o fichário criada com sucesso!!!";
+            }
+            catch (Exception ex)
+            {
+                status = false;
+                mensagem = "Conexão com o fichário criada com erro: " + ex.Message;
+            }
+        }
+    }
+}
