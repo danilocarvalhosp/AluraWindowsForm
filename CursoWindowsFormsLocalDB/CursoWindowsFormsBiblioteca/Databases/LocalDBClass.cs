e07dfc5f473739d3ee7b2100c8a75c5bf436d9e5
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-06-05 16:30:35 -0300

    Obtendo a string de conexão

--- CursoWindowsFormsLocalDB/CursoWindowsFormsBiblioteca/Databases/LocalDBClass.cs
+++ CursoWindowsFormsLocalDB/CursoWindowsFormsBiblioteca/Databases/LocalDBClass.cs
@@ -10,5 +10,14 @@ namespace CursoWindowsFormsBiblioteca.Databases
 {
     public class LocalDBClass
     {
+        public string stringConn;
+        public SqlConnection connDB;
+
+        public LocalDBClass()
+        {
+            stringConn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"E:\\Projetos\\Curso C#\\Alura\\Windows Form\\AluraWindowsForm\\CursoWindowsFormsLocalDB\\CursoWindowsFormsBiblioteca\\Databases\\Fichario.mdf\";Integrated Security=True";
+            connDB = new SqlConnection(stringConn);
+            connDB.Open();
+        }
     }
 }
