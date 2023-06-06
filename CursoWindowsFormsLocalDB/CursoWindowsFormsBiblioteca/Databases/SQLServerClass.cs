afcedce4fc626a80b3ae99e8ade62ce1fee7f1ad
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-06-06 15:03:07 -0300

    Criando a classe de acesso à base de dados SQL Server

---
+++ CursoWindowsFormsLocalDB/CursoWindowsFormsBiblioteca/Databases/SQLServerClass.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CursoWindowsFormsBiblioteca.Databases
+{
+    public class SQLServerClass
+    {
+        public string stringConn;
+        public SqlConnection connDB;
+
+        public SQLServerClass()
+        {
+            try
+            {
+                stringConn = "Data Source=DCSQL\\DANILO;Initial Catalog=ByteBank;Persist Security Info=True;User ID=sa;Password=Dan@8257";
+                connDB = new SqlConnection(stringConn);
+                connDB.Open();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public DataTable SqlQuery(string sql)
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                var myCommand = new SqlCommand(sql, connDB);
+                myCommand.CommandTimeout = 0;
+                var myReader = myCommand.ExecuteReader();
+                dt.Load(myReader);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return dt;
+        }
+
+        public string SqlCommand(string sql)
+        {
+            try
+            {
+                var myCommand = new SqlCommand(sql, connDB);
+                myCommand.CommandTimeout = 0;
+                var myReader = myCommand.ExecuteReader();
+                return "";
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Close()
+        {
+            connDB.Close();
+        }
+
+    }
+}
